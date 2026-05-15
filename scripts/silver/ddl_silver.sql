9af621e4c32d02ce65e249598d156ac4eeef0abb
ty-5 <tylertorsiello@icloud.com>
2026-05-15 12:14:59 -0400

    Create and redefine Silver tables with new column
    This script creates tables in the 'silver' schema and drops existing tables if they already exist. It also adds a new column 'dwh_create_date' for each table.

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -1,3 +1,15 @@
+/*
+===============================================================================
+DDL Script: Create Silver Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'silver' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'silver' Tables
+-- added a new column for each table 'dwh_create_date'
+===============================================================================
+*/
+
 IF OBJECT_ID ('silver.crm_cust_info', 'U') IS NOT NULL
 	DROP TABLE silver.crm_cust_info;
 GO
